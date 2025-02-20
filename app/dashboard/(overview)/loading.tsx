44c7e7cb23078b8b3a7146c06680d1ae11c67efd
jerlibgnzlz <jerlibgnzlz@gmail.com>
2025-02-20 19:58:12 -0300

    fix loading capitulo 8

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,9 @@
+// export default function Loading() {
+//     return <p>Cargando dashboard...</p>;
+// }
+
+import DashboardSkeleton from '@/app/ui/skeletons';
+
+export default function Loading() {
+    return <DashboardSkeleton />;
+}
