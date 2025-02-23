78c7785412a9321f7d6d13142a612487b7bbaea2
jerlibgnzlz <jerlibgnzlz@gmail.com>
2025-02-23 01:22:55 -0300

    producion

--- auth.config.ts
+++ auth.config.ts
@@ -1,3 +1,25 @@
+// import type { NextAuthConfig } from 'next-auth';
+
+// export const authConfig = {
+//     pages: {
+//         signIn: '/login',
+//     },
+//     callbacks: {
+//         authorized({ auth, request: { nextUrl } }) {
+//             const isLoggedIn = !!auth?.user;
+//             const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+//             if (isOnDashboard) {
+//                 if (isLoggedIn) return true;
+//                 return false; // Redirect unauthenticated users to login page
+//             } else if (isLoggedIn) {
+//                 return Response.redirect(new URL('/dashboard', nextUrl));
+//             }
+//             return true;
+//         },
+//     },
+//     providers: [], // Add providers with an empty array for now
+// } satisfies NextAuthConfig;
+
 import type { NextAuthConfig } from 'next-auth';
 
 export const authConfig = {
@@ -10,12 +32,15 @@ export const authConfig = {
             const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
             if (isOnDashboard) {
                 if (isLoggedIn) return true;
-                return false; // Redirect unauthenticated users to login page
+                return false; // Redirigir usuarios no autenticados a /login
             } else if (isLoggedIn) {
                 return Response.redirect(new URL('/dashboard', nextUrl));
             }
             return true;
         },
+        async redirect({ url, baseUrl }) {
+            return baseUrl; // 🔥 Redirigir siempre a `NEXTAUTH_URL`
+        },
     },
-    providers: [], // Add providers with an empty array for now
+    providers: [], // Aquí debes agregar tus proveedores de autenticación
 } satisfies NextAuthConfig;
